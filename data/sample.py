67c4effc9e926b883bbde3594a96fe4ed37d6aac
Tanmay Gupta <tanmay@Tanmays-MacBook-Pro.local>
2016-12-24 02:17:30 +0530

    Fixed comments using emacs 25.1.1

--- data/sample.py
+++ data/sample.py
@@ -4,19 +4,19 @@ import numpy as np
 def sample_training_data(
     num_samples,
     domain):
-#     Args:
-#       - num_samples : Number of training samples to generate
-#       - domain : A list of tuples specifying the range along each dimension
+    # Args:
+    #   - num_samples : Number of training samples to generate
+    #   - domain : A list of tuples specifying the range along each dimension
     
-#     Returns:
-#       - samples : num_samples x len(domain) matrix consisting of samples drawn 
-#           uniformly from the domain
+    # Returns:
+    #   - samples : num_samples x len(domain) matrix consisting of samples drawn 
+    #       uniformly from the domain
  
-#     Example:
-#     To draw 10 samples in R^2 where the first dimension ranges between (-1,1)
-#     and the second dimension between (2,3), the call looks like
+    # Example:
+    # To draw 10 samples in R^2 where the first dimension ranges between (-1,1)
+    # and the second dimension between (2,3), the call looks like
 
-#     samples = sample_training_data(10,[(-1,1),(2,3)])
+    samples = sample_training_data(10,[(-1,1),(2,3)])
 
     num_dims = len(domain)
     samples = np.zeros([num_samples,num_dims])
@@ -30,19 +30,19 @@ def sample_grid_data(
     domain,
     steps,
     reshape=True):
-#     Args:
-#       - domain : A list of tuples specifying the range along each dimension
-#       - steps : A list specifying step size along each dimension
-#       - reshape : If True, also returns the reshaped grid
+    # Args:
+    #   - domain : A list of tuples specifying the range along each dimension
+    #   - steps : A list specifying step size along each dimension
+    #   - reshape : If True, also returns the reshaped grid
 
-#     Returns:
-#       - grid: A list of length len(domain) where grid[i] is an ndarray of 
-#           grid samples along the ith dimension
-#       - samples: Only returned if reshape is True. Returns the grid samples
-#           as a 2D array. 
+    # Returns:
+    #   - grid: A list of length len(domain) where grid[i] is an ndarray of 
+    #       grid samples along the ith dimension
+    #   - samples: Only returned if reshape is True. Returns the grid samples
+    #       as a 2D array. 
 
-#     Example:
-#     grid, samples = sample_grid_data([(-1,1),(2,3)],[0.1,0.3])
+    # Example:
+    # grid, samples = sample_grid_data([(-1,1),(2,3)],[0.1,0.3])
 
     assert(len(domain)==len(steps)), 'domain and steps need to be of the same size'
 
